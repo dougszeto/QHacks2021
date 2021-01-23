427b32dcbbade764a61e1c71a89c4c0aa0d7abe7
Doug Szeto <douglas.szeto@mail.mcgill.ca>
2021-01-23 17:32:23 -0500

    connected front end to back end

--- frontend/src/TinderCards.js
+++ frontend/src/TinderCards.js
@@ -1,18 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TinderCard from 'react-tinder-card';
 import './TinderCards.css';
+import axios from 'axios';
 
 function TinderCards() {
     const [people, setPeople] = useState([
-        {
-            name: 'woofy',
-            url: 'https://www.vets4pets.com/siteassets/species/dog/large-dog-on-walk-looking-over-hills.jpg?w=585&scale=down',
-        },
-        {
-            name: 'fluffy',
-            url: 'https://jooinn.com/images/dog-67.jpg',
-        }
+        // {
+        //     name: 'woofy',
+        //     url: 'https://www.vets4pets.com/siteassets/species/dog/large-dog-on-walk-looking-over-hills.jpg?w=585&scale=down',
+        // },
+        // {
+        //     name: 'fluffy',
+        //     url: 'https://jooinn.com/images/dog-67.jpg',
+        // }
     ]);
+
+    //runs once when the component loads
+    useEffect( () => {
+       axios.get('http://localhost:5000/dogs/')
+        .then(res => {
+            setPeople(res.data);
+        })
+        .catch(err => console.log(`Error receiving from backend: ${err}`));
+    }, []);
+
     return (
         <div>
             <h1>Tinder cards</h1>
@@ -24,7 +35,7 @@ function TinderCards() {
                 preventSwipe={['up', 'down']}
                 >
                     <div
-                    style={{backgroundImage: `url(${person.url})`}}
+                    style={{backgroundImage: `url(${person.image})`}}
                     className='card'
                     >
                         <h3>{person.name}</h3>

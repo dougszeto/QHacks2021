89510eaa76c9b4e3e7abd79bdb6326570fe7d029
ZoeDucroux <zoe.ducroux@mail.mcgill.ca>
2021-01-24 02:03:20 -0500

    message updates

--- frontend/src/Chats.js
+++ frontend/src/Chats.js
@@ -5,33 +5,33 @@ import Chat from './Chat';
 function Chats() {
     return <div className='chats'>
         <Chat
-            name='Mark'
-            message='When do you open on Friday?'
+            name='Sandy'
+            message='Hi my name is Sandy and Im looking for the perfect family'
             timestamp='40 seconds ago'
             profilePic='...'
         />
         <Chat
-            name='Mark'
-            message='When do you open on Friday?'
-            timestamp='40 seconds ago'
+            name='Scruffy'
+            message='I love kids! Im a very playful puppy!'
+            timestamp='1 hour ago'
             profilePic='...'
         />
         <Chat
-            name='Mark'
-            message='When do you open on Friday?'
-            timestamp='40 seconds ago'
+            name='Fido'
+            message='You seem like a good fit for me, Im very calm!'
+            timestamp='2 hours ago'
             profilePic='...'
         />
         <Chat
-            name='Mark'
-            message='When do you open on Friday?'
-            timestamp='40 seconds ago'
+            name='Milo'
+            message='Im well trained and I love treats'
+            timestamp='1 day ago'
             profilePic='...'
         />
         <Chat
-            name='Mark'
+            name='Rooster'
             message='When do you open on Friday?'
-            timestamp='40 seconds ago'
+            timestamp='1 week ago'
             profilePic='...'
         />
     </div>;
